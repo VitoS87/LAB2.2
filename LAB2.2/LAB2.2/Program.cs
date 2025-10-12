e0aa52053237dbf0d95951e1260b45f2c420100f
Vitos <rustamminis@gmail.com>
2025-10-12 16:30:08 +0200

    Add files via upload

---
+++ LAB2.2/LAB2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число ");
+long x = long.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+long y = long.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+long z = long.Parse(Console.ReadLine());
+if (x > y && x < z) { Console.WriteLine("Среднее по значению " + x); }
+if (x < y && x > z) { Console.WriteLine("Среднее по значению " + x); }
+if (y > x && y < z) { Console.WriteLine("Среднее по значению " + y); }
+if (y < x && y > z) { Console.WriteLine("Среднее по значению " + y); }
+if (z > y && z < x) { Console.WriteLine("Среднее по значению " + z); }
+if (z < y && z > x) { Console.WriteLine("Среднее по значению " + z); };
